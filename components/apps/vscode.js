ed3d50ada8b1422b262cd948b088138bbc9a9fcc
Aryan Raj <aryan9639@gmail.com>
2025-07-08 16:39:03 +0530

    second commit

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function VsCode() {
   return (
     <iframe
-      src="https://github1s.com/ANURAG-PATHAK/my-portfolio"
+      src="https://github1s.com/aryanraj-22/Verticloud/blob/main/README.md"
       title="VsCode"
       className="h-full w-full bg-ub-cool-grey"
     ></iframe>
